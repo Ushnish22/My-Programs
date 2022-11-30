fa5324d18458d134130371059c5aa013561fe113
Ushnish22 <ushnish1997>
2022-11-30 18:33:00 +0530

    Second Commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+//console.dir(document.domain);
+//console.log(document.URL);
+//document.title=123;
+
+let x=document.getElementById('main-header');
+let y=document.getElementById('header-title');
+console.log(x);
+x.style.borderBottom='solid 3px #000';
+y.style.textDecorationColor='#555';
